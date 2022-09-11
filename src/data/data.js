6b34afedc170a9329ec4b6bd2b4c3d685b4b5bfe
Sergio Sinuco <ssinuco@gmail.com>
2022-09-10 21:05:52 -0500

    react app - integration by ssi

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+const data = {
+  tractors: {
+    eicher: {
+      label: 'Eicher Diesel 215/16',
+      img: 'https://mi-fr.org/img/eicher.svg',
+    },
+    fendt: {
+      label: 'Fendt F20 Dieselroß',
+      img: 'https://mi-fr.org/img/fendt.svg',
+    },
+    porsche: {
+      label: 'Porsche-Diesel Master 419',
+      img: 'https://mi-fr.org/img/porsche.svg',
+    }
+  }
+};
+
+export default data;
