7c6d2ac9fea3697ff790c0fe71a39462ac3bfda1
Sergio Sinuco <ssinuco@gmail.com>
2022-09-10 21:05:52 -0500

    react app - integration by iframe

---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,20 @@
+import styles from './ProductDetail.module.css';
+import data from '../../data/data';
+import { useParams } from 'react-router-dom';
+
+const ProductDetail = () => {
+  const params = useParams();
+  const productId = params.productId;
+  const product = data.tractors[productId];
+
+  return <div class={styles.layout}>
+    <h1 class={styles.header}>The Tractor Store</h1>
+    <div class={styles.product}>
+      <h2>{product.label}</h2>
+      <img class={styles.image} src={product.img} />
+    </div>
+    <aside class={styles.recos}></aside>
+  </div>
+}
+
+export default ProductDetail;
